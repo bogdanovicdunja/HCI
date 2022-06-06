51e0a2a9e0cdd896fb96905944bbf2b0edf14b52
Dunja Bogdanovic <dunjabogdanovic00@gmail.com>
2022-06-06 16:51:38 +0200

    day off requests added to frontend

---
+++ bolnica/bolnica/Model/DayOffRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bolnica.Model
+{
+    public class DayOffRequest
+    {
+        public uint Id { get; set; }
+        public string DoctorName { get; set; }
+       // public string DoctorSurname { get; set; }
+        public string Specialization { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+
+        public DayOffRequest()
+        {
+
+        }
+
+        public DayOffRequest(uint id, string doctorName, string specialization, string start, string end)
+        {
+            Id = id;
+            DoctorName = doctorName;
+            Specialization = specialization;
+            Start = start;
+            End = end;
+        }
+
+        public DayOffRequest(string doctorName, string specialization, string start, string end)
+        {
+            DoctorName = doctorName;
+            Specialization = specialization;
+            Start = start;
+            End = end;
+        }
+    }
+}
--- bolnica/bolnica/Pages/DayOff.xaml.cs
+++ bolnica/bolnica/Pages/DayOff.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using bolnica.Model;
+using System;
+
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,30 @@ namespace bolnica.Pages
     /// </summary>
     public partial class DayOff : Page
     {
+
+        public ObservableCollection<DayOffRequest> DayOffList { get; set; }
         public DayOff()
         {
             InitializeComponent();
+            GRD.Items.Clear();
+
+            DayOffRequest day1 = new DayOffRequest(1, "Dunja", "Cardiology", "13/07/2022", "17/07/2022");
+            DayOffRequest day2 = new DayOffRequest(2, "Olivera", "Oftalmology", "22/06/2022", "24/06/2022");
+            DayOffRequest day3 = new DayOffRequest(3, "David", "Neurology", "09/08/2022", "15/09/2022");
+            DayOffRequest day4 = new DayOffRequest(4, "Marko", "Dermatology", "10/10/2022", "11/10/2022");
+
+            DataContext = this;
+
+            //DayOffList.Add(day1);
+            //DayOffList.Add(day2);
+            //DayOffList.Add(day3);
+            //DayOffList.Add(day4);
+
+            GRD.Items.Add(day1);
+            GRD.Items.Add(day2);
+            GRD.Items.Add(day3);
+            GRD.Items.Add(day4);
+
         }
 
         private void Accept_Click(object sender, RoutedEventArgs e)
